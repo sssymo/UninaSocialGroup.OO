9ba4b7aaac2cebb41b7f27816230e9cc2d9697cd
luigi <luigi@LAPTOP-JJNS3RPE>
2024-02-29 16:15:18 +0100

    Commit 29-02

---
+++ src/classiDao/UserDao.java
@@ -0,0 +1,29 @@
+package classiDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+    private Connection connection;
+
+    public UserDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean authenticateUser(String username, String password) throws SQLException{
+        String query = "SELECT * FROM Users WHERE username = ? AND password = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, username);
+            statement.setString(2, password);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next(); // Se c'è almeno una riga corrispondente, l'autenticazione è avvenuta con successo
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; // Gestione dell'eccezione: ritorna false in caso di errore di accesso al database
+        }
+    }
+}
+
