dff062eaef5cbefa9da38115c0d9c3cc309b5ac3
luigi <luigi@LAPTOP-JJNS3RPE>
2024-03-05 22:06:05 +0100

    modifica piccolezze

--- src/driver/main.java
+++ src/driver/main.java
@@ -5,21 +5,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import database.DbConnection;
 import controller.Controller;
-<<<<<<< HEAD
-public class main {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		DbConnection dbconn = null;
-		Connection connection = null;
-		try {
-			dbconn = DbConnection.getDBConnection();
-			connection = dbconn.getConnection();
-
-			Controller c = new Controller(connection);
 			
-=======
-
 public class main {
 
 	public static void main(String[] args) throws IOException {
@@ -31,7 +18,6 @@ public class main {
 			connection = dbconn.getConnection();
 
 			Controller c = new Controller(connection);
->>>>>>> branch 'main' of https://github.com/sssymo/UninaSocialGroup.OO.git
 
 		} catch (SQLException e) {
 			e.printStackTrace();
